81311bf3b4ed250c3be5afa54db2e17a4fee6ac3
Kevin Islas <kevin.islas.abud@gmail.com>
2025-08-05 19:44:38 -0700

    Update bonk.lua

--- bonk.lua
+++ bonk.lua
@@ -2,10 +2,16 @@ function bonk ()
   print("bonk")
   turtle.select(2)
   turtle.place()
+  turtle.sleep(1)
+  turtle.place()
 end
-while true do
+
+local bonked = false
+
+while not bonked do
   local i = math.random(1, 10)
   if i == 1 then
     bonk()
+    bonked = true
   end
 end
