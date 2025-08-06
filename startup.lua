b9cdfdd4bc85bb4227b1b956b9eb3648decdc3cf
Kevin Islas <kevin.islas.abud@gmail.com>
2025-08-05 17:17:43 -0700

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,11 +1,26 @@
-while true do
-  -- Pause the program until a redstone event happens
-  os.pullEvent("redstone")
-  
-  -- Check if the back of the turtle is receiving a redstone signal
-  if redstone.getInput("front") then
-    print("Redstone signal DETECTED!")
-    turtle.select(1)
-    turtle.place()
-  end
+
+local speaker = peripheral.find("speaker")
+local dfpwm = require("cc.audio.dfpwm")
+
+function playAudio (audio_path)
+  local decoder = dfpwm.make_decoder()
+  for chunk in io.lines(audio_path, 16 * 1024) do
+    local buffer = decoder(chunk)
+    while not speaker.playAudio(buffer) do
+      os.pullEvent("speaker_audio_empty")
+    end
 end
+
+  playAudio("cobra.dfpwm")
+  
+-- while true do
+--   -- Pause the program until a redstone event happens
+--   os.pullEvent("redstone")
+  
+--   -- Check if the back of the turtle is receiving a redstone signal
+--   if redstone.getInput("front") then
+--     print("Redstone signal DETECTED!")
+--     turtle.select(1)
+--     turtle.place()
+--   end
+-- end
